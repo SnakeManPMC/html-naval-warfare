a086d24b5c6d65f5e1fdf1c9f834c754598e7b98
Snake Man <snakeman@phnet.fi>
2024-10-13 22:49:22 +0000

    my first commit, yippy

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Naval Warfare and Fleet Tactics - PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Naval Warfare and Fleet Tactics enthusiast website by PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Naval Warfare PMC Tactical</h1>
+<p>Welcome to Naval Warfare enthusiast website by PMC.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+	<h2>Naval Warfare Fleet Tactics</h2>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p><a href="links.php">Links</a>, back to <a href="index.php">Naval Warfare PMC Tactical root page</a></p>
+<br><br>
+<p><i>Naval Warfare PMC Tactical 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
---
+++ links.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Links Naval Warfare PMC Tactical</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="Links Naval Warfare PMC Tactical">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php include("/var/www/pmctactical.org/include/google-analytics.php"); ?>
+
+</head>
+<body>
+
+<header>
+<?php include("/var/www/include/ads.php"); ?>
+<?php include("/var/www/include/header-start.php"); ?>
+	<h1>Links Naval Warfare PMC Tactical</h1>
+<p>Useful web links Naval Warfare enthusiast website by PMC Tactical.</p>
+<?php include("/var/www/include/header-end.php"); ?>
+</header>
+
+<section>
+<?php include("/var/www/include/section-start.php"); ?>
+
+<?php include("/var/www/include/links.php"); ?>
+
+<?php include("/var/www/include/section-end.php"); ?>
+</section>
+
+<footer>
+<?php include("/var/www/include/footer-start.php"); ?>
+<p>Back to <a href="index.php">Naval Warfare PMC Tactical root page</a></p>
+<br><br>
+<p><i>Naval Warfare PMC Tactical 2024 - <?php print(date("Y")); ?>.</i></p>
+<?php include("/var/www/include/footer-end.php"); ?>
+<?php include("/var/www/include/support.php"); ?>
+<?php include("/var/www/include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
