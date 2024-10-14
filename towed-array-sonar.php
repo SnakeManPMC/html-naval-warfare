33969005096600a3ec14eb5a16d7d4092f30a907
Snake Man <snakeman@phnet.fi>
2024-10-14 00:53:13 +0000

    towed-array-sonar page created

--- towed-array-sonar.php
+++ towed-array-sonar.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Naval Warfare and Fleet Tactics - PMC Tactical</title>
+<title>Towed Array Sonar Naval Warfare and Fleet Tactics - PMC Tactical</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="Naval Warfare and Fleet Tactics enthusiast website by PMC Tactical">
+<META name="description" content="Towed Array Sonar Naval Warfare and Fleet Tactics enthusiast website by PMC Tactical">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
@@ -15,23 +15,18 @@
 <header>
 <?php include("/var/www/include/ads.php"); ?>
 <?php include("/var/www/include/header-start.php"); ?>
-	<h1>Naval Warfare PMC Tactical</h1>
+	<h1>Towed Array Sonar Naval Warfare PMC Tactical</h1>
 <p>Welcome to Naval Warfare enthusiast website by PMC.</p>
 <?php include("/var/www/include/header-end.php"); ?>
 </header>
 
 <section>
 <?php include("/var/www/include/section-start.php"); ?>
-	<h2>Naval Warfare Fleet Tactics</h2>
+	<h2>Towed Array Sonar</h2>
 
-<p><a href="about.php">About</a> us or well this website, what is it, what its purpose etc.</p>
-<p><a href="anti-submarine-warfare.php">Anti Submarine Warfare (ASW)</a> hunting of enemy submarines.</p>
-<p><a href="dipping-sonar.php">Dipping Sonar</a> is a sonar device you lower from hovering ASW helicopter.</p>
-<p><a href="emcon.php">EMCON</a> Emissions Control, shut your radar/sonar off.</p>
-<p><a href="magnetic-anomaly-detector.php">Magnetic Anomaly Detector</a> or in short MAD.</p>
-<p><a href="sonobuoy.php">Sonobuoy</a> you drop them in the ocean to listen sounds, mainly submarines.</p>
-<p><a href="thermocline.php">Thermocline</a> or thermal layer in the ocean temperatures.</p>
-<p><a href="towed-array-sonar.php">Towed Array Sonar</a> a cable towed behind a submarine or surface ship with sonar at the end of it.</p>
+<p>
+<i>A towed array sonar is a system of hydrophones towed behind a submarine or a surface ship on a cable.</i> -source <a href="https://en.wikipedia.org/wiki/Towed_array_sonar" target="_blank">towed array sonar wikipedia</a>.
+</p>
 
 <?php include("/var/www/include/section-end.php"); ?>
 </section>
